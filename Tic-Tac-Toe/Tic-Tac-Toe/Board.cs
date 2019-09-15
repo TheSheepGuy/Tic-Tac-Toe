04c3ac5bd77f46020116606c5499b36644e62e49
TheSheepGuy <TheSheepGuy1@gmail.com>
2019-09-15 23:05:54 +0100

    Finished drawing the board and making a move.

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Board.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    class Board
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public List<Space> Spaces { get; set; }
+
+        public Board(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Spaces = new List<Space>();
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    Spaces.Add(new Space(i, j));
+                }
+            }
+        }
+
+        public void DrawBoard()
+        {
+            Console.WriteLine();
+            for (int i = Height-1; i >= 0; i--)
+            {
+                Console.Write("\n+---+---+---+");
+                Console.Write("\n|   |   |   |");
+                Console.Write($"\n{i}");
+                for (int j = 0; j < Width; j++)
+                {
+                    //The "find" expression finds the space at the current X and Y position.
+                    Console.Write($" {(Spaces.Find(a => a.XCoordinate == j && a.YCoordinate == i)).State} |");
+                }
+                Console.Write("\n|   |   |   |");
+            }
+            Console.Write("\n+-0-+-1-+-2-+");
+        }
+
+        public void MakeMove(int x, int y, int player)
+        {
+            var spaceState = Spaces.Find(a => a.XCoordinate == x && a.YCoordinate == y);
+            if (player == 1)
+            {
+                spaceState.State = "X";
+            }
+            else
+            {
+                spaceState.State = "O";
+            }
+            
+        }
+    }
+}
--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -6,7 +6,31 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("===TIC-TAC-TOE===");
+            // Create the game board.
+            var gameBoard = new Board(3, 3);
+            // Keep track of the game's state, whether it has ended yet,
+            bool end = false;
+            // Record what player won. 0 is no-one yet, 1 player 1, 2 player 2, 3 a draw.
+            int playerWin = 0;
+            int currentPlayer = 1;
+            // A variable just to hold temporary user input, initially empty.
+            var input = new ConsoleKeyInfo();
+
+            while(end == false)
+            {
+                for (int i = 1; i < 3; i++)
+                {
+                    gameBoard.DrawBoard();
+                    Console.WriteLine($"\nPlease enter the x-coordinate of your move, Player {i}");
+                    input = Console.ReadKey();
+                    int x = int.Parse(input.KeyChar.ToString());
+                    Console.WriteLine($"\nPlease enter the y-coordinate");
+                    input = Console.ReadKey();
+                    int y = int.Parse(input.KeyChar.ToString());
+                    gameBoard.MakeMove(x, y, i);
+                }
+            }
         }
     }
 }
